57c43203b16428ef7db83073bfbca3c08550cf70
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-08-03 16:45:42 +0300

    Backend: Created drizzle-kit migrations

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,6 +2,6 @@ import "dotenv/config";
 
 export const ENV = {
     PORT: process.env.PORT || 5001,
-    DATABASE_URL: process.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
 };
